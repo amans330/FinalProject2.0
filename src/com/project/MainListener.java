5eb0d58c1fa25500eb6f4e1378ef8614d728b4d8
adp7 <46548107+adp7@users.noreply.github.com>
2019-03-11 21:06:37 -0700

    add search code main functionality, url patterns

--- src/com/project/MainListener.java
+++ src/com/project/MainListener.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,20 +43,34 @@ public class MainListener extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		ArrayList<Books> data;
+		if(request.getHttpServletMapping().getPattern().contains("Search")){
+			String searchstring = request.getParameter("searchstring"); 
+			data = MySQLAccess.getBooksByName(searchstring);
+		}else {
+			String genre = request.getParameter("genre"); 
+			
+			data = MySQLAccess.getBooksForGenre(genre);
+		}
+		request.setAttribute("booklist", data);
+		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+		rd.include(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getHttpServletMapping().getPattern();
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		if(MySQLAccess.checkCredentials(username, password) == true) {
 			request.setAttribute("username", username);
 			request.getRequestDispatcher("/index.jsp").forward(request, response); 
-//			response.sendRedirect("/Finalproject2/index.jsp");
 		}else {
-			response.getWriter().print("wrong things");
+			RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
+			response.getWriter().println("<font color=red>Wrong user name or password </font>");
+			rd.include(request, response);
 		}
 	}
 
