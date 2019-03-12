e6798f9f3e849df2fe5d4dc8855017b6dbc03a44
amans330 <amans330@gmail.com>
2019-03-11 21:52:41 -0700

    books table, web.xml

---
+++ src/com/project/Books.java
@@ -0,0 +1,41 @@
+package com.project;
+
+public class Books {
+	int bookid;
+	String bookname;
+	String author_name;
+	String genre;
+	String rating;
+	
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getAuthor_name() {
+		return author_name;
+	}
+	public void setAuthor_name(String author_name) {
+		this.author_name = author_name;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+}
